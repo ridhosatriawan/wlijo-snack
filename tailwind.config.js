63fcb55fd1e0d08de32130ca3be92620845a3cd6
ridhosatriawan <ridhosatriawan321@gmail.com>
2022-02-26 00:12:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.html"],
+  theme: {
+    extend: {
+      height: {
+        270: "270px",
+      },
+    },
+    colors: {
+      green1: "#40916C",
+      green2: "#74C69D",
+      black1: "#161616",
+      gray1: "#DBDBDB",
+      gray2: "#EDEDED",
+      white: "#FFFFFF",
+    },
+  },
+  plugins: [],
+};
